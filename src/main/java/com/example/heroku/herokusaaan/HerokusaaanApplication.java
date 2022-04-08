a09badcd06190d1201c3c1925b53aa273b719007
IMARGHIO <ioan.marghioala@capgemini.com>
2022-04-08 12:46:54 +0300

    Commit for HEROKU-SAN

---
+++ src/main/java/com/example/heroku/herokusaaan/HerokusaaanApplication.java
@@ -0,0 +1,13 @@
+package com.example.heroku.herokusaaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokusaaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokusaaanApplication.class, args);
+	}
+
+}
